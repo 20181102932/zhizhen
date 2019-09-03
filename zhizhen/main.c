bbb9f5a421cfea9ab89ea5e3ca74c3ce7dbe6ac1
20181102932 <908907990@qq.com>
2019-09-03 16:02:23 +0800

    Initial Commit

---
+++ zhizhen/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  zhizhen
+//
+//  Created by s20181102932 on 2019/9/3.
+//  Copyright © 2019 wjx. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
