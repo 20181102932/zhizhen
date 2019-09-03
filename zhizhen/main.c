4400e04f407b4bcf29d466d8ba89dee3339c48a3
20181102932 <908907990@qq.com>
2019-09-03 16:59:10 +0800

    zhi zhen pai xu

--- zhizhen/main.c
+++ zhizhen/main.c
@@ -5,11 +5,33 @@
 //  Created by s20181102932 on 2019/9/3.
 //  Copyright © 2019 wjx. All rights reserved.
 //
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+int main( )
+{
+    int a[5]={0};
+    int i=0,j=0,middle=0;
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int *x=a;
+     for(i=0;i<4;i++)
+     {
+         for(j=0;j<4;j++)
+         {
+             if(a[j]>a[j+1])
+             {
+                 middle=a[j+1];
+                 a[j+1]=*x;
+                 a[j]=middle;
+             }
+             x++;
+         }
+            x=a;
+     }
+     for(i=0;i<5;i++)
+     {
+         printf("%d ",a[i]);
+     }
     return 0;
 }
